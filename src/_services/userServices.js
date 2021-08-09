42d57134da6d639f0d473a22d383324812fba9ac
Matthew Kirby <matt.kirby@yahoo.com>
2021-08-08 21:33:30 -0400

    added comments to userServices

--- src/_services/userServices.js
+++ src/_services/userServices.js
@@ -11,7 +11,9 @@ function changeUser(id) {
   return fetch(`${apiUrl}/users/${id}`, requestOptions)
     .then((response) => response.json())
     .then((user) => {
+      // once the new user has been fetched, set local storage to that users stringified data
       localStorage.setItem("user", JSON.stringify(user));
+      // return the user data to the userAction
       return user;
     });
 }
