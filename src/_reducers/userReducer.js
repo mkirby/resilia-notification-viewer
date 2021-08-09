b64d5f3df92635949430683eb6817f8b4185d717
Matthew Kirby <matt.kirby@yahoo.com>
2021-08-08 21:16:23 -0400

    added comments to userReducer

--- src/_reducers/userReducer.js
+++ src/_reducers/userReducer.js
@@ -1,12 +1,15 @@
 import { userConstants } from "../_constants/userConstants";
 
+// if there is a currentUser logged in localStorage and parse it out
 let user = JSON.parse(localStorage.getItem("user"));
+// then set it as the initial state
+// if there's no currentUser instantiate a empty object
 const initialState = user ? { ...user } : {};
 
 export function userReducer(state = initialState, action) {
   switch (action.type) {
     case userConstants.CHANGE_SUCCESS:
-      // this is an object that will go through combinereducers to become { user: {userData} }
+      // action.userData is an object that will go through combinereducers to become { user: {userData} }
       return action.userData;
     default:
       return { ...state };
